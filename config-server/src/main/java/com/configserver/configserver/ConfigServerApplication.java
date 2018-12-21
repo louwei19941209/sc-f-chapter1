219d2964290587a1688aa020c261ebff175bc38c
louwei19941209 <546929255@qq.com>
2018-12-21 15:26:17 +0800

    Config Server

---
+++ config-server/src/main/java/com/configserver/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.configserver.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer //开启配置服务器的功能
+@SpringBootApplication
+public class ConfigServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
+
