885afc3db45efafdb151f4a6ae4f4b0773f94b89
louwei19941209 <546929255@qq.com>
2018-12-21 11:36:51 +0800

    测试提交

---
+++ eureka-server/src/main/java/com/eureka/HelloWord.java
@@ -0,0 +1,15 @@
+package com.eureka;
+
+/**
+ * @program: sc-f-chapter1
+ * @description: hello word
+ * @author: lou wei
+ * @create: 2018-12-21 11:34
+ **/
+public class HelloWord
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hell word");
+    }
+}
